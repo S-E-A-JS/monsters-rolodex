cb90f37278138da392d4872b03bdeff5a643d93a
S-E-A-JS <9168441394@bk.ru>
2022-10-09 20:56:35 +0600

    learning STATE SETSTATE on class Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: {
+        firstName: "Igor",
+        lastName: "Levirov",
+      },
+      company: "ZTM",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World</h1>
+
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <p>
+            Hello, my name is {this.state.name.firstName} {this.state.name.lastName}, i work at{" "}
+            {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState(
+                (state, props) => {
+                  return {
+                    name: {
+                      firstName: "Egor",
+                      lastName: "Samoylov",
+                    },
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                },
+              );
+              // console.log(this.state);
+            }}
+          >
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
